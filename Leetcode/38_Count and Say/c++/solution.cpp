f6f34a68f77340b9092f2daff0fe180d90637fbb
njumathdy <15850788569@163.com>
2019-04-03 21:08:27 +0800

    4.3 2nd

---
+++ Leetcode/38_Count and Say/c++/solution.cpp
@@ -0,0 +1,37 @@
+/*******************
+*******************/
+#include <cstdlib>
+#include <cstdio>
+#include <exception>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// 题目本身表述的有些不清楚，just for fun
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n == 0) return "";
+        string res = "1";
+        while (--n) {
+            string cur = "";
+            for (int i = 0; i < res.size(); i++) {
+                int count = 1;
+                while ((i + 1 < res.size()) && (res[i] == res[i + 1])){
+                    count++;    
+                    i++;
+                }
+                cur += to_string(count) + res[i];
+            }
+            res = cur;
+        }
+        return res;
+    }
+};
+
+int main() {
+    return 0;
+}
